80e4b881a29410b2ab1f7f101fd3eb08616e77d8
sabialhyv <sabialhyv@gmail.com>
2024-10-24 14:01:32 +0400

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import AllCountries from "./components/AllCountries";
 import CountryInfo from "./components/CountryInfo";
 
 function App() {
-  return (
-    <>
-      <div className="header">
-        <div className="container">
-          <h5>Where in the world</h5>
-        </div>
-      </div>
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<AllCountries />} />
-          <Route path="/country/:countryName" element={<CountryInfo />} />
-        </Routes>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="header">
+                <div className="container">
+                    <h5>Where in the world?</h5>
+                </div>
+            </div>
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<AllCountries />} />
+                    <Route path="/country/:countryName" element={<CountryInfo />} />
+                </Routes>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
 
 const SearchInput = ({ onSearch }) => {
-    const [input, setInput] = useState("");
+  const [input, setInput] = useState("");
 
-    const submitHandler = (e) => {
-        e.preventDefault();
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-        onSearch(input);
-    };
+    onSearch(input);
+  };
 
-    return (
-        <form onSubmit={submitHandler}>
-            <input
-                type="text"
-                placeholder="Search a country......"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-            />
-        </form>
-    );
+  return (
+    <form onSubmit={submitHandler}>
+      <input
+        type="text"
+        placeholder="Search a country......"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+    </form>
+  );
 };
 
 export default SearchInput;
