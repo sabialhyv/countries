426f00c3f914969a1efa4faa21c3ff7982c2f31b
sabialhyv <sabialhyv@gmail.com>
2024-10-24 14:55:21 +0400

    modify

--- src/components/CountryInfo.jsx
+++ src/components/CountryInfo.jsx
@@ -1,73 +1,54 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { apiURL } from "./util/api";
-import { Link } from "react-router-dom";
 
 const CountryInfo = () => {
   const [country, setCountry] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState("");
-
   const { countryName } = useParams();
 
-  const borders = country.map((country) => country.borders);
-
   useEffect(() => {
-    const getCountryByName = async () => {
+    0
+    const fetchCountry = async () => {
       try {
         const res = await fetch(`${apiURL}/name/${countryName}`);
-
-        if (!res.ok) throw new Error("Could not found!");
-
+        if (!res.ok) throw new Error("Country not found!");
         const data = await res.json();
-
-        setCountry(data);
-        setIsLoading(false);
+        setCountry(data[0]);
       } catch (error) {
-        setIsLoading(false);
         setError(error.message);
+      } finally {
+        setIsLoading(false);
       }
     };
-
-    getCountryByName();
+    fetchCountry();
   }, [countryName]);
+
+  if (isLoading) return <h4>Loading...</h4>;
+  if (error) return <h4>{error}</h4>;
 
   return (
     <div className="country-info-wrapper">
       <button>
         <Link to="/">Back</Link>
       </button>
-
-      {isLoading && !error && <h4>Loading...</h4>}
-      {error && !isLoading && { error }}
-
-      {country?.map((country, index) => (
-        <div className="country-info-container" key={index}>
+      {country && (
+        <div className="country-info-container">
           <div className="country-info-img">
-            <img src={country.flags.png} alt="" />
+            <img src={country.flags.png} alt={`${country.name.common} flag`} />
           </div>
-
           <div className="country-info">
             <h3>{country.name.common}</h3>
-
             <div className="country-info-left">
-              <h5>
-                Population:{" "}
-                <span>
-                  {new Intl.NumberFormat().format(country.population)}
-                </span>
-              </h5>
-              <h5>
-                Region: <span>{country.region}</span>
-              </h5>
-              {country.subregion ? <h5>Sub Region: <span>{country.subregion}</span></h5> : null}
-              <h5>
-                Capital: <span>{country.capital}</span>
-              </h5>
+              <h5>Population: <span>{new Intl.NumberFormat().format(country.population)}</span></h5>
+              <h5>Region: <span>{country.region}</span></h5>
+              {country.subregion && <h5>Sub Region: <span>{country.subregion}</span></h5>}
+              <h5>Capital: <span>{country.capital?.[0]}</span></h5>
             </div>
           </div>
         </div>
-      ))}
+      )}
     </div>
   );
 };
